5aad0435e4c79c6e8d6b3d9725f736ebff9d25c7
Isaac <isaacsabino70@gmail.com>
2025-04-16 13:29:11 -0400

    Week 7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        
+        activities.Add(new Running("12 May 2018", 30, 4.8));
+        activities.Add(new Cycling("9 Jun 2018", 45, 15.5));
+        activities.Add(new Swimming("18 Jul 2018", 60, 40));
+
+        
+        foreach (Activity act in activities)
+        {
+            Console.WriteLine(act.GetSummary());
+        }
     }
 }
